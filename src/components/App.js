ff0773d3793cc55dbde0aa7b4e8c6ae1207ec6a1
Hamza Hajji <hmzhjji@gmail.com>
2017-11-06 16:27:34 +0000

    add functionality

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,31 @@ import Header from './Header';
 import Pad from './Pad';
 
 class App extends Component {
+  state = {
+    currOp: '',
+  }
+
+  onClick(e) {
+    this.setState({
+      currOp: `${this.state.currOp}${e.target.value}`
+    });
+  }
+
+  addDecimal(e) {
+    let lastChar = this.state.currOp[this.state.currOp.length - 1]
+    if ("*-+/".indexOf(lastChar) === -1) {
+      this.setState({
+        currOp: `${this.state.currOp}${e.target.value}`
+      });
+    }
+  }
+
+  performCalc() {
+    this.setState({
+      currOp: String(eval(this.state.currOp))
+    });
+  }
+
   render() {
     return (
       <div className="container">
@@ -12,7 +37,11 @@ class App extends Component {
           <Header />
         </div>
         <div className="row">
-          <Pad />
+          <Pad
+            addDecimal={this.addDecimal.bind(this)}
+            onClick={this.onClick.bind(this)}
+            performCalc={this.performCalc.bind(this)}
+            {...this.state} />
         </div>
       </div>
     );
--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,40 +1,46 @@
 import React from 'react';
 
-export default () => {
+export default ({
+  onClick,
+  performCalc,
+  addDecimal,
+  currOp
+}) => {
   return (
     <div className=" col-8 col-sm-6 col-lg-4 mx-auto">
       <table>
         <thead>
           <tr>
             <td colSpan="4">
-              <div id="result"></div>
+              <div id="result"><h3>{currOp.length ? currOp : 0}</h3></div>
             </td>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td><button className="num">1</button></td>
-            <td><button className="num">2</button></td>
-            <td><button className="num">3</button></td>
-            <td><button id="plus">+</button></td>
+            <td><button value="1" onClick={onClick}>1</button></td>
+            <td><button value="2" onClick={onClick}>2</button></td>
+            <td><button value="3" onClick={onClick}>3</button></td>
+            <td><button value="+" onClick={onClick}>+</button></td>
           </tr>
           <tr>
-            <td><button className="num">4</button></td>
-            <td><button className="num">5</button></td>
-            <td><button className="num">6</button></td>
-            <td><button id="minus">-</button></td>
+            <td><button value="4" onClick={onClick}>4</button></td>
+            <td><button value="5" onClick={onClick}>5</button></td>
+            <td><button value="6" onClick={onClick}>6</button></td>
+            <td><button value="-" onClick={onClick}>-</button></td>
           </tr>
           <tr>
-            <td><button className="num">7</button></td>
-            <td><button className="num">8</button></td>
-            <td><button className="num">9</button></td>
-            <td><button id="times">&times;</button></td>
+            <td><button value="7" onClick={onClick}>7</button></td>
+            <td><button value="8" onClick={onClick}>8</button></td>
+            <td><button value="9" onClick={onClick}>9</button></td>
+            <td><button value="*" onClick={onClick}>&times;</button></td>
           </tr>
           <tr>
-            <td><button>.</button></td>
-            <td><button>0</button></td>
-            <td><button>=</button></td>
-            <td><button id="div">&#247;</button></td>
+            { /*add validation to decimal, prevent from adding after operation*/ }
+            <td><button value="." onClick={addDecimal}>.</button></td>
+            <td><button  value="0" onClick={onClick}>0</button></td>
+            <td><button onClick={performCalc}>=</button></td>
+            <td><button value="/" onClick={onClick}>&#247;</button></td>
           </tr>
         </tbody>
       </table>
